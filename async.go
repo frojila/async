693684eeb6cde43cef9740b34e0bdafdcb2bae65
Mario Raspiantoro <raspiantoro@gmail.com>
2022-11-05 15:44:34 +0700

    feat(awaitgroup): wait multiple future
    add abilities to wait multiple future at once

--- async.go
+++ async.go
@@ -6,22 +6,35 @@ type ReturnType[T any] func() (T, error)
 
 type AsyncFunc[Fn ReturnType[T], T any] func() Fn
 
-type Future[Fn ReturnType[T], T any] struct {
+// since golang uses interface implicitly, this line will provide static compile time check that ensure future always satisfied Awaitable
+// the _ will tell the compiler to discard this line, so we can ensure there is no unneeded extra allocation
+var _ Awaitable = &future[ReturnType[any], any]{}
+
+type future[Fn ReturnType[T], T any] struct {
 	sigend chan struct{}
 	ret    Fn
 }
 
+// awaitSigend implements Awaitable
+func (f *future[Fn, T]) awaitSigend() <-chan struct{} {
+	return f.sigend
+}
+
+func (f *future[Fn, T]) Await() Fn {
+	<-f.sigend
+	return f.ret
+}
+
+func (f *future[Fn, T]) Retrieve() Fn {
+	return f.ret
+}
+
 type runner[Fn ReturnType[T], T any] struct {
-	future *Future[Fn, T]
+	future *future[Fn, T]
 	runFn  AsyncFunc[Fn, T]
 }
 
-func (r *Future[Fn, T]) Await() Fn {
-	<-r.sigend
-	return r.ret
-}
-
-func (r *runner[Fn, T]) run() *Future[Fn, T] {
+func (r *runner[Fn, T]) run() *future[Fn, T] {
 	go func() {
 		r.future.ret = r.runFn()
 		r.future.sigend <- struct{}{}
@@ -30,9 +43,9 @@ func (r *runner[Fn, T]) run() *Future[Fn, T] {
 	return r.future
 }
 
-func Go[Fn ReturnType[T], T any](f AsyncFunc[Fn, T]) *Future[Fn, T] {
+func Go[Fn ReturnType[T], T any](f AsyncFunc[Fn, T]) *future[Fn, T] {
 	r := &runner[Fn, T]{
-		future: &Future[Fn, T]{
+		future: &future[Fn, T]{
 			sigend: make(chan struct{}),
 		},
 		runFn: f,
